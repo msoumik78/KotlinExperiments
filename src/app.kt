afdd2971255198b2b561df5d57c4e35043c70e47
Soumik Mukherjee <msoumik78@gmail.com>
2019-09-02 01:46:32 +0530

    updated with null safe check and data class

--- src/app.kt
+++ src/app.kt
@@ -9,6 +9,10 @@ fun main() {
     println("Hello world from Kotlin !")
     println("Your firstName is : $firstName, and surname : $lastName and age is $age")
 
+    nullSafetyDemonstration()
+
+    dataClassUsage()
+
     calculateSalaryBasedOnAgeAndDepartment("HR", 30);
 
     // Calling the same function with named arguments and hence order is not necessary
@@ -24,6 +28,29 @@ fun main() {
 
     println("Department of emp object : ${emp.department}")
 }
+
+
+fun dataClassUsage(){
+    // The below line is equivalent to some 20 lines in Java to declare a class with all getters and setters and equals & hashcode methods
+    data class Student (val name: String, val age: Int, val school: String)
+
+    var studentObject= Student ("Soumik", 40, "South Point")
+    println("Student name is : ${studentObject.name} and age is ${studentObject.age} and school is : ${studentObject.school} ")
+
+}
+
+
+fun nullSafetyDemonstration() {
+    var name : String = "Soumik"
+    //below line generates a compile time error
+    //name = null
+
+    var surname: String? = "Mukherjee"
+    // below line does NOT generate a compile type error as we have used a nullable type
+    surname= null
+}
+
+
 
 // Example of a function with default arguments, the second argument age has a default value 40
 fun calculateSalaryBasedOnAgeAndDepartment(department: String, age: Int = 40) : Double {
